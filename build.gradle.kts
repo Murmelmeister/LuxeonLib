31d376a420440d596ae90879d31dd6a69515f4cf
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-10-24 23:08:35 +0200

    feat: add SLF4J simple logger dependency to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 
 dependencies {
     implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
