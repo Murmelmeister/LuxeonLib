eec63afc9e1a94fff2f45713b63c055ad765085b
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-10-24 20:29:37 +0200

    feat: update build configuration for Java 21 and add HikariCP dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,20 @@
 plugins {
-    id("java")
+    java
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 group = "de.murmelmeister.luxeonlib"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.zaxxer:HikariCP:7.0.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
